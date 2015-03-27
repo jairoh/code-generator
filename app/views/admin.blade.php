00a04480952a50d7b3e5d778d8f0446a71c0cd77
root <jairohtuada@gmail.com>
2015-03-27 09:47:58 +0800

    Add keyword hint colors in problem description [admin page]; add edit problem description popup [admin page].

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -23,17 +23,19 @@
 
   <table class="problem_list table" >
     <tr class="tr_green" >
-      <td colspan="2" ><b>Easy Problems</b></td>
+      <td colspan="3" ><b>Manage [Easy Problems]</b></td>
     </tr>
     <tr>
-      <td>Level</td>
-      <td>Description</td>
+      <td><b>Level</b></td>
+      <td><b>Description</b></td>
+      <td></td>
     </tr>
 
     <?php foreach( $easy_problems as $problem ): ?>
     <tr>
-      <td>{{$problem->level}}</td>
+      <td><b>{{$problem->level}}</b></td>
       <td>{{$problem->description}}</td>
+      <td><button type="button" data-target="#myModal" data-toggle="modal" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></button></td>
     </tr>
     <?php endforeach; ?>
 
@@ -47,18 +49,20 @@
 
   <table class="problem_list table" >
     <tr class="tr_blue" >
-      <td colspan="2" ><b>Average Problems</b></td>
+      <td colspan="3" ><b>Manage [Average Problems]</b></td>
     </tr>
 
     <tr>
-      <td>Level</td>
-      <td>Description</td>
+      <td><b>Level</b></td>
+      <td><b>Description</b></td>
+      <td></td>
     </tr>
 
     <?php foreach( $average_problems as $problem ): ?>
     <tr>
-      <td>{{$problem->level}}</td>
+      <td><b>{{$problem->level}}</b></td>
       <td>{{$problem->description}}</td>
+      <td></td>
     </tr>
     <?php endforeach; ?>
 
@@ -66,5 +70,56 @@
   </table>
 
   
+
+  <!-- Edit user modal -->
+  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+          <h5 class="modal-title" id="myModalLabel">Update <span>Easy</span> Problem Level <span>1</span> </h5>
+        </div>
+        <div class="modal-body">
+          
+          <table class="table table-user-information">
+            
+            <tr>
+              <td>
+                For hint keywords, wrap the text with: <br />
+                <vs>variable(s)</vs> = <b><?= htmlentities( "<vs>...</vs>" )?></b> <br />
+                <ps>print statement(s)</ps> = <b><?= htmlentities( "<ps>...</ps>" )?></b> <br />
+                <cond>conditional statement(s)</cond> = <b><?= htmlentities( "<cond>...</cond>" )?></b> <br />
+                <ws>while statement(s)</ws> = <b><?= htmlentities( "<ws>...</ws>" )?></b> <br /> 
+                <dws>do while statement(s)</dws> = <b><?= htmlentities( "<dws>...</dws>" )?></b> <br />
+                <fls>for loop statement(s)</fls> = <b><?= htmlentities( "<fls>...</fls>" )?></b> <br />
+                <ss>switch statement(s)</ss> = <b><?= htmlentities( "<ss>...</ss>" )?></b> <br />
+
+                <br>
+                To add a nextline, put <b><?= htmlentities( "<br>" )?></b>
+              </td>
+            </tr>
+
+            <tr>
+             <td><textarea id="text_area_problem_descrp" ></textarea></td>
+            </tr>
+
+          </table>
+
+          <div id="update_errors" ></div>
+
+        </div>
+        
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" id="update_password_btn" >Save changes</button>
+          </div>
+
+
+      </div>
+    </div>
+  </div>
+  <!-- End edit user modal -->
+
+
 
 @stop
